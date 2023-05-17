10008998fc49b851b240677757fb179cef8152cc
ebede2g <108631764+ebede2g@users.noreply.github.com>
2023-05-18 02:33:36 +0300

    v1.7 є генерація випадкових перешкод групами, лише не на далекі відстані.

--- app/src/main/java/com/example/oseg2/DemoRender.java
+++ app/src/main/java/com/example/oseg2/DemoRender.java
@@ -2,7 +2,6 @@ package com.example.oseg2;
 
 import android.opengl.GLSurfaceView;
 import android.opengl.GLU;
-import android.util.Log;
 
 import com.example.oseg2.Objects.Object_triangle_Obstacle;
 import com.example.oseg2.Objects.Object_tunel;
@@ -22,7 +21,7 @@ class DemoRenderer implements GLSurfaceView.Renderer {
 
 
     private static float scaleFctor = 1;
-    private static float speed = 0.0521f;
+    private static float speed = 0.121f;
 
 
     private static int obs_counter = 0;
@@ -30,7 +29,7 @@ class DemoRenderer implements GLSurfaceView.Renderer {
     private static float tick;
     private static float deadline;
 
-    private static int next_tick_of_obs=-10;
+    private static int next_tick_of_obs = -10;
 
 
     private void createObs(GL10 gl, Integer obs_dist, Integer obs_pos) {
@@ -48,27 +47,54 @@ class DemoRenderer implements GLSurfaceView.Renderer {
         }
     }
 
-    {int min_loop_1_type = 3;
-        int max_loop_1_type = 12;
-        int min_loop_2_type = 2;
-        int max_loop_2_type = 7;
-        int min_rand_3_type = 6;
-        int max_rand_3_type = 3;} // еонстанти
-    private void randomGenObs(GL10 gl){
-        if(tick<next_tick_of_obs) {
-            switch (randNum(1,3)){
-                case (1): {
-                    int start_pos = randNum(0,15);
-                    for(int gen = start_pos;gen<randNum(start_pos+3,start_pos+12);gen++){
-                        createObs(gl,next_tick_of_obs,gen);
-                    }
+
+        int que1;int que1_pos;int que1_amnt_t1;int que1_amnt_t2;int que1_amnt_t3;
+
+        int que2;int que2_pos;int que2_amnt_t1;int que2_amnt_t2;int que2_amnt_t3;
+
+
+
+    private void randomGenObs(GL10 gl) {
+        if (tick < next_tick_of_obs - 1) {
+            next_tick_of_obs = -randNum(-2, 2) - (int) ((int) 24 + 16 * Math.sin(Math.sin(Math.pow(-tick, 1.1) - 2 * -tick))) + next_tick_of_obs; //майбутні координати перешкоди
+
+            que1 = randNum(1, 3);
+            que1_pos = randNum(0, 15);
+
+            que1_amnt_t1 = randNum(que1_pos + 3, que1_pos + 12);
+
+            que1_amnt_t2 = randNum(que1_pos + 2, que1_pos + 5);
+
+            que1_amnt_t3 = randNum(que1_pos + 1, 8);
+
+
+
+        }
+
+
+        switch (que1) {
+            case (1): {
+                for (int gen = que1_pos; gen < que1_amnt_t1; gen++) {
+                    createObs(gl, -next_tick_of_obs, gen);
                 }
             }
-
-
-            next_tick_of_obs = -randNum(-2,2)-(int) ((int) 24 + 16 * Math.sin(Math.sin(Math.pow(-tick, 1.1) - 2 * -tick))) + next_tick_of_obs; //майбутні координати перешкоди
-            Log.d("beb","next = "+next_tick_of_obs);
+            case (2): {
+                for (int gen = que1_pos; gen < que1_amnt_t2; gen++) {
+                    createObs(gl, -next_tick_of_obs, gen);
+                }
+                for (int gen = que1_pos + que1_amnt_t2 + 2; gen < que1_amnt_t2 * 2 + 1; gen++) {
+                    createObs(gl, -next_tick_of_obs, gen);
+                }
+            }
+            case (3): {
+                for (int gen = que1_pos; gen < que1_amnt_t3 * 2; gen += 2) {
+                    createObs(gl, -next_tick_of_obs, gen);
+                }
+            }
+            default: ;
         }
+
+
     }
 
     @Override
@@ -84,6 +110,7 @@ class DemoRenderer implements GLSurfaceView.Renderer {
         gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
 
     }
+
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         // Sets the current view port to the new size.
@@ -99,6 +126,7 @@ class DemoRenderer implements GLSurfaceView.Renderer {
         // Reset the modelview matrix
         gl.glLoadIdentity();
     }
+
     @Override
     public void onDrawFrame(GL10 gl) {
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
@@ -108,17 +136,6 @@ class DemoRenderer implements GLSurfaceView.Renderer {
         gl.glTranslatef(0.0f, 0.81f, -tick);
         gl.glRotatef(tick_rot, 0.0f, 0.0f, 1.0f);
         tunel.draw(gl);
-
-
-        createObs(gl, 10, 0);
-        createObs(gl, 20, 0);
-        createObs(gl, 30, 0);
-        createObs(gl, 40, 0);
-
-
-
-
-        //Log.d("beb","tick = "+tick);
 
         tick += duConst * speed;
         tick_rot += x_rot;
